287ad6f7ee44bce6f606ec11349c27387cdbcb2a
Michal Drda <drdam@students.zcu.cz>
2018-11-05 22:41:12 +0100

    minor fixes

--- src/main/java/drdm.school.pia/web.servlet.spring/Main.java
+++ src/main/java/drdm.school.pia/web.servlet.spring/Main.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
-@WebServlet("/main")
+@WebServlet({"/main", "/manage", "/banking"})
 public class Main extends AbstractServlet {
 
     private SecretManager SecretManager;
